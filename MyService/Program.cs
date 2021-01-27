e70a1e31db9d3e06ce7a3e4f9b29afdcfc6422a8
Kazuki Ota <kaota@microsoft.com>
2021-01-27 12:24:49 +0900

    プロジェクト ファイルを追加します。

---
+++ MyService/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System.ServiceProcess;
+
+namespace MyService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceBase.Run(new MyService());
+        }
+    }
+
+    class MyService : ServiceBase
+    {
+        protected override void OnStart(string[] args)
+        {
+            using (var sw = new StreamWriter("c:\\temp\\myservice.log", true))
+            {
+                sw.WriteLine("OnStart");
+            }
+            base.OnStart(args);
+        }
+
+        protected override void OnStop()
+        {
+            using (var sw = new StreamWriter("c:\\temp\\myservice.log", true))
+            {
+                sw.WriteLine("OnStop");
+            }
+            base.OnStop();
+        }
+    }
+}
